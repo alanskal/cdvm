213c4ea05429f388c199ba2d1af0709320b1512d
SKALSKI Alan <AlanSkalski@outlook.fr>
2024-08-15 18:45:54 +0200

    archives work images

--- src/pages/archives/index.tsx
+++ src/pages/archives/index.tsx
@@ -16,20 +16,36 @@ export default function Archives() {
             <p className='text-center'>Les vêtements rehaussés par le Conservatoire de la mode vintage représentent l'excellence du prêt-à-porter français de jadis. Les marques collectionnées et archivées ont joué un rôle clé dans l'évolution de l'industrie de la mode hexagonale.
           </p>
         </div>
-        <div className="grid grid-cols-2 grid-row-2 gap-6 w-[400px] mx-auto">
 
-            <Link href='/archives/60'>
-              <div className="col-span-1 row-span-1 h-[200px] relative">
-                <Image src="/images/archives/archive1.jpg" layout='fill' alt="archive1" className='rounded'/>
+      <div className="container mx-auto pt-4">
+
+        <div className="grid grid-cols-4 grid-rows-2 gap-x-5 gap-y-6">
+
+          <Link href='/archives/60' className='relative row-span-1 mx-[32px]'>
+            <div className="">
+              <Image src="/images/archives/archive1.jpg" layout='fill' objectFit='fill' alt="archive1" className='rounded absolute'/>
+            </div>
+            </Link>
+            <div className="col-span-1 row-span-1 ">
+              <p className='self-center'>La mode des années 1960 est marquée par un renversement complet des préceptes établis lors de la précédente décennie. La Haute Couture voit sa suprématie supplantée par le prêt-à-porter, porté par un vent de libération. La jeunesse devient à la fois source d’inspiration et client final du vêtement.
+              </p>
+              </div>
+
+            <div className="col-span-1 row-span-1 ">
+              <p className='text-right self-center'>Dans les années 1970, la mode occidentale se caractérise par deux grandes tendances : les vêtements fantaisie et les tenues pratiques du prêt-à-porter. Les vêtements unisexe, surtout les jeans, gagnent en popularité. Les styles hippie, punk, glam-rock et disco marquent cette période.
+              </p>
+            </div>
+            <Link href='/archives/70' className='relative col-span-1 row-span-1 mx-[32px]'>
+              <div className="col-span-1 row-span-1 h-[200px]">
+                <Image src="/images/archives/archive2.jpg" layout='fill' alt="archive2"className='rounded absolute'/>
               </div>
             </Link>
+        </div>
+      </div>
 
-            <Link href='/archives/70'>
-              <div className="col-span-1 row-span-1 h-[200px] relative">
-                <Image src="/images/archives/archive2.jpg" layout='fill' alt="archive2"className='rounded'/>
-              </div>
-            </Link>
 
+
+            {/* <div>
             <Link href='/archives/80'>
               <div className="col-span-1 row-span-1 h-[200px] relative">
                 <Image src="/images/archives/archive3.PNG" layout='fill' alt="archive3"className='rounded'/>
@@ -41,8 +57,8 @@ export default function Archives() {
                 <Image src="/images/archives/archive4.jpg" layout='fill' alt="archive4"className='rounded'/>
               </div>
             </Link>
+            </div> */}
 
-        </div>
     </div>
   )
 }

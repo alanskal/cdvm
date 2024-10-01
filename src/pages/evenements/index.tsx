81dbccba8f2a33680024a427cf96f9f378478b18
SKALSKI Alan <AlanSkalski@outlook.fr>
2024-10-01 19:23:37 +0200

    fin

---
+++ src/pages/evenements/index.tsx
@@ -0,0 +1,15 @@
+export default function EventsComing() {
+  return (
+  <section>
+    <div>
+      <h2 className="text-red">NOS ÉVENEMENTS</h2>
+      <p>Suivez toute l’actualité du Conservatoire de la mode vintage, et participez à nos prochains
+      événements !</p>
+    </div>
+    <div>
+
+    </div>
+  </section>
+
+  )
+}
